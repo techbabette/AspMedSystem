02893ff1e31d17541e88ddfe9edee2e084147bfc
techbabette <marinakrsticrf@gmail.com>
2024-06-07 21:32:53 -0700

    [FEAT] Initialize Domain and DataAccess projects

---
+++ AspMedSystem/AspMedSystem.DataAccess/MedSystemContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspMedSystem.DataAccess
+{
+    public class MedSystemContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

22360c7a3b4d4eea3b3160dee597182c196c8685
techbabette <marinakrsticrf@gmail.com>
2024-06-09 02:48:18 -0700

    [FEAT] Treatment entity

---
+++ AspMedSystem/AspMedSystem.DataAccess/Configurations/TreatmentConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using AspMedSystem.Domain;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspMedSystem.DataAccess.Configurations
+{
+    internal class TreatmentConfiguration : NamedEntityConfiguration<Treatment>
+    {
+        protected override void ConfigureEntity(EntityTypeBuilder<Treatment> builder)
+        {
+            base.ConfigureEntity(builder);
+        }
+    }
+}
---
+++ AspMedSystem/AspMedSystem.DataAccess/Migrations/20240609094643_Treatments.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AspMedSystem.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Treatments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Treatment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Treatment", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Treatment_Name",
+                table: "Treatment",
+                column: "Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Treatment");
+        }
+    }
+}

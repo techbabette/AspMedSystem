34908842866e3ab62737994300dcfa2b2b4dab2e
techbabette <marinakrsticrf@gmail.com>
2024-06-10 03:27:55 -0700

    [FEAT] EfUseCase

---
+++ AspMedSystem/AspMedSystem.Implementation/UseCases/EfUseCase.cs
@@ -0,0 +1,21 @@
+﻿using AspMedSystem.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspMedSystem.Implementation.UseCases
+{
+    public abstract class EfUseCase
+    {
+        private readonly MedSystemContext _context;
+
+        protected EfUseCase(MedSystemContext context)
+        {
+            _context = context;
+        }
+
+        protected MedSystemContext Context => _context;
+    }
+}

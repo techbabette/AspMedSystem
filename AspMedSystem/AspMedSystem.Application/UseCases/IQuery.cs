29b803c000a4bb1fa97d2106212c14e34141551a
techbabette <marinakrsticrf@gmail.com>
2024-06-10 03:36:27 -0700

    [FEAT] Application use case interfaces

---
+++ AspMedSystem/AspMedSystem.Application/UseCases/IQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspMedSystem.Application.UseCases
+{
+    public interface IQuery<TResult, TSearch> : IUseCase where TResult : class
+    {
+        TResult Execute(TSearch search);
+    }
+}

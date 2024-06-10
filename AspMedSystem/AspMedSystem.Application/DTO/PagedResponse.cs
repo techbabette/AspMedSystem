f4cd14cefcde46d27435fdea0bb8131cfec15982
techbabette <marinakrsticrf@gmail.com>
2024-06-10 03:46:19 -0700

    [FEAT] Add PagedResponse/PagedSearch

---
+++ AspMedSystem/AspMedSystem.Application/DTO/PagedResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspMedSystem.Application.DTO
+{
+    public class PagedResponse<TDTO>
+        where TDTO : class
+    {
+        public IEnumerable<TDTO> Data { get; set; }
+        public int PerPage { get; set; }
+        public int TotalCount { get; set; }
+        public int Pages
+        {
+            get
+            {
+                return (int)Math.Ceiling((double)TotalCount / PerPage);
+            }
+        }
+        public int CurrentPage { get; set; }
+    }
+}

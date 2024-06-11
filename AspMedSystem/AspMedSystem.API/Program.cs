52ae97192946acb222baad581a5c808e056a5897
techbabette <marinakrsticrf@gmail.com>
2024-06-11 07:26:59 -0700

    [FEAT] Register exception handling middleware

--- AspMedSystem/AspMedSystem.API/Program.cs
+++ AspMedSystem/AspMedSystem.API/Program.cs
@@ -1,3 +1,5 @@
+using AspMedSystem.API.Core;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +10,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())

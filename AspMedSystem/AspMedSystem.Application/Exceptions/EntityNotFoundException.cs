42b66b79c1970ec8bdbb9200aefadd7dd9075f07
techbabette <marinakrsticrf@gmail.com>
2024-06-11 07:23:35 -0700

    [FEAT] Exception handling middleware

---
+++ AspMedSystem/AspMedSystem.Application/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspMedSystem.Application.Exceptions
+{
+    public class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException(string entityType, int id) :
+            base($"Entity of type {entityType} with an id of {id} doesn't exist.")
+        {
+        }
+    }
+}

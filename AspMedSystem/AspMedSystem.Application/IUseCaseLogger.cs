5d31832d1ef517106cfc2adf3b26a8a9f07d6e9b
techbabette <marinakrsticrf@gmail.com>
2024-06-12 07:10:59 -0700

    [FEAT] DB UseCaseLogger

---
+++ AspMedSystem/AspMedSystem.Application/IUseCaseLogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspMedSystem.Application
+{
+    public interface IUseCaseLogger
+    {
+        void Log(UseCaseLog log);
+    }
+    public class UseCaseLog
+    {
+        public string UserEmail { get; set; }
+        public string UseCaseName { get; set; }
+        public object UseCaseData { get; set; }
+    }
+}
---
+++ AspMedSystem/AspMedSystem.Implementation/Logging/UseCases/DbUseCaseLogger.cs
@@ -0,0 +1,25 @@
+﻿using AspMedSystem.Application;
+using AspMedSystem.DataAccess;
+using Newtonsoft.Json;
+namespace AspMedSystem.Implementation.Logging.UseCases
+{
+    public class DbUseCaseLogger : IUseCaseLogger
+    {
+        private readonly MedSystemContext _context;
+
+        public DbUseCaseLogger(MedSystemContext context)
+        {
+            _context = context;
+        }
+
+        public void Log(UseCaseLog log)
+        {
+            _context.UseCaseLogs.Add(new Domain.UseCaseLog
+            {
+                Data = JsonConvert.SerializeObject(log.UseCaseData),
+                ActorEmail = log.ActorEmail,
+                UseCaseName = log.UseCaseName
+            });
+        }
+    }
+}

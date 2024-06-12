12ca6601e4af36a8272ba53bf7f76e2bc09ec52d
techbabette <marinakrsticrf@gmail.com>
2024-06-11 21:34:10 -0700

    [FEAT] JWT Actor provider

---
+++ AspMedSystem/AspMedSystem.Implementation/Actor.cs
@@ -0,0 +1,26 @@
+﻿using AspMedSystem.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspMedSystem.Implementation
+{
+    public class Actor : IApplicationActor
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public IEnumerable<string> AllowedUseCases { get; set; }
+    }
+}
---
+++ AspMedSystem/AspMedSystem.Implementation/UnauthorizedActor.cs
@@ -0,0 +1,26 @@
+﻿using AspMedSystem.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspMedSystem.Implementation
+{
+    public class UnauthorizedActor : IApplicationActor
+    {
+        public int Id => 0;
+
+        public string Username => "unauthorized";
+
+        public string Email => "/";
+
+        public string FirstName => "unauthorized";
+
+        public string LastName => "unauthorized";
+
+        public DateTime BirthDate => DateTime.UtcNow;
+
+        public IEnumerable<string> AllowedUseCases => new List<string> { };
+    }
+}

45dec93b668922300ed02d8b2aaa267d7c9f3768
techbabette <marinakrsticrf@gmail.com>
2024-06-13 01:52:47 -0700

    [FEAT] Clean up use case controller

--- AspMedSystem/AspMedSystem.API/Controllers/UseCasesController.cs
+++ AspMedSystem/AspMedSystem.API/Controllers/UseCasesController.cs
@@ -15,30 +15,5 @@ namespace AspMedSystem.API.Controllers
         {
             return Ok(UseCaseInfo.AllUseCases);
         }
-
-        // GET api/<UseCasesController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<UseCasesController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<UseCasesController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<UseCasesController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }

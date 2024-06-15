4bdf37e644823ff7c45091798f0a3a710bde1987
techbabette <marinakrsticrf@gmail.com>
2024-06-15 05:49:53 -0700

    [FEAT] Users start

---
+++ AspMedSystem/AspMedSystem.Application/DTO/UserSearchSingleDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspMedSystem.Application.DTO
+{
+    public class UserSearchSingleDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Group { get; set; }
+        public IEnumerable<string> GroupAllowedUseCases { get; set; }
+        public IEnumerable<string> UserAllowedUseCases { get; set; }
+        public IEnumerable<string> UserDisallowedUseCases { get; set; }
+    }
+}
---
+++ AspMedSystem/AspMedSystem.Implementation/UseCases/Commands/Users/EfUserSearchSelfQuery.cs
@@ -0,0 +1,55 @@
+﻿using AspMedSystem.Application.DTO;
+using AspMedSystem.Application.Exceptions;
+using AspMedSystem.Application.UseCases.Commands.Users;
+using AspMedSystem.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspMedSystem.Implementation.UseCases.Commands.Users
+{
+    public class EfUserSearchSelfQuery : EfUseCase, IUserSearchSingleQuery
+    {
+        private EfUserSearchSelfQuery()
+        {
+        }
+
+        public EfUserSearchSelfQuery(MedSystemContext context) : base(context)
+        {
+        }
+
+        public string Name => "Show information about user";
+
+        public UserSearchSingleDTO Execute(int search)
+        {
+            var user = Context.Users.Where(user => user.Id == search)
+                                    .Include(user => user.Group)
+                                    .ThenInclude(group => group.GroupPermissions)
+                                    .Include(user => user.UserPermissions)
+                                    .FirstOrDefault();
+
+            if(user == null)
+            {
+                throw new EntityNotFoundException("User", search);
+            }
+
+            var dto = new UserSearchSingleDTO()
+            {
+                Email = user.Email,
+                Group = user.Group.Name,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                BirthDate = user.BirthDate,
+                Id = user.Id,
+                UserAllowedUseCases = user.UserPermissions.Where(permission => permission.Effect == Domain.Effect.Allow).Select(x => x.Permission).ToList(),
+                UserDisallowedUseCases = user.UserPermissions.Where(permission => permission.Effect == Domain.Effect.Disallow).Select(x => x.Permission).ToList(),
+                GroupAllowedUseCases = user.Group.GroupPermissions.Where(permission => permission.Effect == Domain.Effect.Allow).Select(x => x.Permission).ToList(),
+            };
+
+            return dto;
+        }
+    }
+}

bc27996527581e62008d186c479b4f0dd94c0e62
techbabette <marinakrsticrf@gmail.com>
2024-06-16 10:41:42 +0200

    [FEAT] Update term start

---
+++ AspMedSystem/AspMedSystem.Implementation/UseCases/Commands/ExaminationTerms/EfExaminationTermUpdateCommand.cs
@@ -0,0 +1,29 @@
+﻿using AspMedSystem.Application.DTO;
+using AspMedSystem.Application.UseCases.Commands.ExaminationTerms;
+using AspMedSystem.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspMedSystem.Implementation.UseCases.Commands.ExaminationTerms
+{
+    internal class EfExaminationTermUpdateCommand : EfUseCase, IExaminationTermUpdateCommand
+    {
+        private EfExaminationTermUpdateCommand()
+        {
+        }
+
+        public EfExaminationTermUpdateCommand(MedSystemContext context) : base(context)
+        {
+        }
+
+        public string Name => "Update examination term";
+
+        public void Execute(ExaminationTermUpdateDTO data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

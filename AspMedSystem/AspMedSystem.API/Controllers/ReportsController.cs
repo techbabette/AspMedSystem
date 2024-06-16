a17de801a5e67c14a5d96da23b1d351f313e7977
techbabette <marinakrsticrf@gmail.com>
2024-06-16 15:58:15 +0200

    [FEAT] Report controller init

---
+++ AspMedSystem/AspMedSystem.API/Controllers/ReportsController.cs
@@ -0,0 +1,50 @@
+﻿using AspMedSystem.Implementation;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspMedSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportsController : ControllerBase
+    {
+        private readonly UseCaseHandler hander;
+
+        // GET: api/<ReportsController>
+        public ReportsController(UseCaseHandler hander)
+        {
+            this.hander = hander;
+        }
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ReportsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ReportsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ReportsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ReportsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ AspMedSystem/AspMedSystem.Application/DTO/ReportCreateDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspMedSystem.Application.DTO
+{
+    public class ReportCreateDTO
+    {
+        public int ExaminationId { get; set; }
+        public string Text { get; set; }
+    }
+}

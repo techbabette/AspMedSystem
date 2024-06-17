6cbbda41e3dbf49225797f9116374289ffbd0c0a
techbabette <marinakrsticrf@gmail.com>
2024-06-17 11:17:13 +0200

    [FEAT] Treatment commands dto and interfaces

---
+++ AspMedSystem/AspMedSystem.API/Controllers/TreatmentsController.cs
@@ -0,0 +1,50 @@
+﻿using AspMedSystem.Implementation;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspMedSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TreatmentsController : ControllerBase
+    {
+        private readonly UseCaseHandler handler;
+
+        public TreatmentsController(UseCaseHandler handler)
+        {
+            this.handler = handler;
+        }
+        // GET: api/<TreatmentsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<TreatmentsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<TreatmentsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<TreatmentsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<TreatmentsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

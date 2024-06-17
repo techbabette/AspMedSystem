9511568a645125b430a9a9bf783cb9ce25706ab8
techbabette <marinakrsticrf@gmail.com>
2024-06-17 11:30:38 +0200

    [FEAT] Declare treatment search parameters

---
+++ AspMedSystem/AspMedSystem.Application/DTO/TreatmentSearchDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspMedSystem.Application.DTO
+{
+    public class TreatmentSearchDTO : PagedSearch
+    {
+        public string Name { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+    }
+
+    public class TreatmentSearchResultDTO
+    {
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+    public class TreatmentSearchResultSingleDTO : TreatmentSearchResultDTO
+    {
+        public int NumberOfTimesPrescribed { get; set; }
+    }
+}
